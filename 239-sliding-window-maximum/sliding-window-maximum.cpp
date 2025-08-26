e3fb2ea268a62c154c9c4b2d9a345faae085a505
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2025-08-26 16:02:52 +0530

    Time: 21 ms (67.93%) | Memory: 139.2 MB (60.28%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,13 +4,15 @@ public:
         vector<int> result;
         deque<int> dq;
         for(int i=0; i<nums.size(); i++){
-            if(!dq.empty() && dq.front()==i-k)
-                dq.pop_front();
-            while(!dq.empty() && nums[dq.back()] <nums[i])
+            if(!dq.empty() && dq.front()<=i-k)
+            dq.pop_front();
+            while(!dq.empty() && nums[dq.back()]<nums[i]){
                 dq.pop_back();
-            dq.push_back(i);
-            if(i>=k-1)
+            }
+             dq.push_back(i);
+            if(i>=k-1){
                 result.push_back(nums[dq.front()]);
+            }           
         }
         return result;
     }
